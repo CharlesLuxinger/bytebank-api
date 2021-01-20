96a575445dbf5d4d5f244afe6065468084db5b7d
Charles Luxinger <charlesluxinger@gmail.com>
2021-01-20 19:41:29 -0300

    Initializing Project

---
+++ src/main/java/com/github/charlesluxinger/bytebank/ByteBankChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.github.charlesluxinger.bytebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteBankChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteBankChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/charlesluxinger/bytebank/ByteBankChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.charlesluxinger.bytebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteBankChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
